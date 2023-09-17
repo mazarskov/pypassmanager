7538063145b221de44b5a34daa97327480127eec
mazarskov <125377291+mazarskov@users.noreply.github.com>
2023-09-17 13:03:16 +0300

    Add files via upload

--- interface.py
+++ interface.py
@@ -37,23 +37,34 @@ def encrypt_button_event():
     user_input_bytes = bytes(user_input, encoding='utf-8')
     aes_key_bytes = bytes(aes_key, encoding='utf-8')
     result = Encryptor_AES(user_input_bytes, aes_key_bytes)
+    username = entry3.get()
+    website = entry4.get()
     print(result)
     f = open('readme.txt', 'a')
     #f.write(str(count))
     #f.write('\n')
+    f.write("--------------------------------")
+    f.write('\n')
+    f.write("Website: " + website)
+    f.write('\n')
+    f.write("Username: " + username)
+    f.write('\n')
     f.write(result)
     f.write('\n')
+    f.write("--------------------------------")
     f.close()
     #count = count + 1
     #print(count)
     entry.delete(0, 'end')
+    entry4.delete(0, 'end')
+    entry3.delete(0, 'end')
 encrypt_button = customtkinter.CTkButton(app, text="Encrypt", command=encrypt_button_event)
-encrypt_button.place(relx=0.5, rely=0.9, anchor=customtkinter.CENTER)
+encrypt_button.place(relx=0.5, rely=0.45, anchor=customtkinter.CENTER)
     
 
 #entry where the key gets generated to
 entry2 = customtkinter.CTkEntry(app, placeholder_text="CTkEntry", state='readonly')
-entry2.place(relx = 0.1, rely = 0.3, anchor=customtkinter.CENTER)
+entry2.place(relx = 0.1, rely = 0.7, anchor=customtkinter.CENTER)
 
 
 #fucntion of a button that generates a key
@@ -63,7 +74,7 @@ def keygen_button_event():
     entry2.insert(0, generate_key())
     entry2.configure(state='readonly')
 keygen_button = customtkinter.CTkButton(app, text="Generate key", command=keygen_button_event)
-keygen_button.place(relx=0.1, rely=0.2, anchor=customtkinter.CENTER)
+keygen_button.place(relx=0.1, rely=0.6, anchor=customtkinter.CENTER)
 
 
 #function of a button that copies the generated key
@@ -72,7 +83,7 @@ def copy_key_button_event():
     app.clipboard_clear()
     app.clipboard_append(text_to_copy)
 copy_key_button = customtkinter.CTkButton(app, text="Copy Key", command=copy_key_button_event, height=10, width=50)
-copy_key_button.place(relx=0.1, rely=0.4, anchor=customtkinter.CENTER)
+copy_key_button.place(relx=0.1, rely=0.8, anchor=customtkinter.CENTER)
 
 
 #fucntion of a button that decrypts using an encrypted text and a proper AES key
@@ -80,7 +91,7 @@ def decrypt_button_event():
     result = Decryptor(entry_encrypted.get(), entry_key.get())
     print(result)
 decrypt_button = customtkinter.CTkButton(app, text="Decrypt", command=decrypt_button_event)
-decrypt_button.place(relx=0.7, rely=0.9, anchor=customtkinter.CENTER)    
+decrypt_button.place(relx=0.5, rely=0.9, anchor=customtkinter.CENTER)    
 
 
 #entry to input an AES key
@@ -92,12 +103,20 @@ entry_key.place(relx = 0.5, rely = 0.3, anchor=customtkinter.CENTER)
 entry = customtkinter.CTkEntry(app, placeholder_text="Password")
 entry.place(relx = 0.5, rely = 0.2, anchor=customtkinter.CENTER)
 
+#entry to input username
+entry3 = customtkinter.CTkEntry(app, placeholder_text="Username")
+entry3.place(relx = 0.3, rely = 0.2, anchor=customtkinter.CENTER)
+
+#entry to put website
+entry4 = customtkinter.CTkEntry(app, placeholder_text="Website")
+entry4.place(relx = 0.7, rely = 0.2, anchor=customtkinter.CENTER)
+
 
 
 
 #entry to input encrypted text
-entry_encrypted = customtkinter.CTkEntry(app, placeholder_text="Encrypted text")
-entry_encrypted.place(relx = 0.7, rely = 0.2, anchor=customtkinter.CENTER)
+entry_encrypted = customtkinter.CTkEntry(app, placeholder_text="Encrypted text", width=350)
+entry_encrypted.place(relx = 0.5, rely = 0.8, anchor=customtkinter.CENTER)
 
 
 
